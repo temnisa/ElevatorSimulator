85eb13b6dc1f6694ef980fca7398c970cfbf09e4
temnisa <kanikamasaikou.jin@hotmail.co.jp>
2026-07-21 14:40:07 +0900

    feat: implement MOVING_UP state in Elevator_Update

--- src/elevator.c
+++ src/elevator.c
@@ -55,20 +55,36 @@ void Elevator_Update(ElevatorData* elevator) {
 		if (elevator->move_timer >= 10) {
 			// 1秒経過したら階数を上げる
 			elevator->current_floor++;
-			// タイマーを初期化
+			// 移動タイマーを初期化
 			elevator->move_timer = 0;
 
 			// 目標階に到達したらドアを開ける
 			if (elevator->current_floor == elevator->target_floor) {
 				elevator->current_state = DOOR_OPEN;
-				// ドアを開けるタイマーを初期化
+				// ドアタイマーを初期化
 				elevator->door_timer = 0;
 			}
-
 		}
-
 		break;
 
+	case MOVING_DOWN:
+
+		elevator->move_timer++;
+
+		if (elevator->move_timer >= 10) {
+			// 1秒経過したら階数を下げる
+			elevator->current_floor--;
+			// 移動タイマーを初期化
+			elevator->move_timer = 0;
+
+			// 目標階に到達したらドアを開ける
+			if (elevator->current_floor == elevator->target_floor) {
+				elevator->current_state = DOOR_OPEN;
+				// ドアタイマーを初期化
+				elevator->door_timer = 0;
+			}
+		}
+		break;
 
 	default:
 		break;
